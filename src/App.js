3dccde659cb188f649c4cd996e2de724ef92a6c8
Girish Pawar <girishpawar.dev@gmail.com>
2022-07-09 10:20:25 +0530

    navbar from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
+import Navbar from'./components/home/Navbar';
 
 let firstName = 'G I R I S H';
 let lastName = 'P A W A R';
 function App() {
   return (
-    <>
       <div className="App">
+        <Navbar/>
         <header className="App-header">
           <p>
             <b>{firstName}&nbsp;&nbsp;&nbsp;&nbsp;{lastName}</b>
           </p>
         </header>
       </div>
-    </>
   );
 }
 
